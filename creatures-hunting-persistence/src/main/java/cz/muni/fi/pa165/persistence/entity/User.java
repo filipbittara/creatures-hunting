aba16102c49d06e5aca41aa7b740a19cf97e1d07
Ondrej Klein <ondra.klein@gmail.com>
2015-10-28 17:32:16 +0100

    get/set methods

--- creatures-hunting-persistence/src/main/java/cz/muni/fi/pa165/persistence/entity/User.java
+++ creatures-hunting-persistence/src/main/java/cz/muni/fi/pa165/persistence/entity/User.java
@@ -28,4 +28,32 @@ public class User {
 	private String username;
 	
 	private String password;
+	
+		public Long getId() {
+		return id;
+	}
+
+	public int getRole() {
+		return role;
+	}
+
+	public void setRole(int role) {
+		this.role = role;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }

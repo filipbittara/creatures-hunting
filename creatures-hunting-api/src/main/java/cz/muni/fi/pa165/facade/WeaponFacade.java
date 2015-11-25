93f9e4a643694d0f1e5bc01d3d37293fcf95ab9e
Ondrej Klein <ondra.klein@gmail.com>
2015-11-25 23:46:27 +0100

    weapon facade interface

--- creatures-hunting-api/src/main/java/cz/muni/fi/pa165/facade/WeaponFacade.java
+++ creatures-hunting-api/src/main/java/cz/muni/fi/pa165/facade/WeaponFacade.java
@@ -1,9 +1,21 @@
 package cz.muni.fi.pa165.facade;
 
+import cz.muni.fi.pa165.dto.WeaponDTO;
+import cz.muni.fi.pa165.dto.CreatureDTO;
+import java.util.Set;
+
 /**
  *
  * @author Ondrej Klein
  */
 public interface WeaponFacade {
-	
+	public void addWeapon(WeaponDTO weapon);
+	public void updateWeapon(WeaponDTO weapon);
+	public void assignToCreature(WeaponDTO weapon, CreatureDTO creature);
+	public void removeFromCreature(WeaponDTO weapon, CreatureDTO creature);
+	public void removeWeapon(WeaponDTO weapon);
+	public WeaponDTO getWeaponById(Long id);
+	public WeaponDTO getWeaponByName(String name);
+	public Set<CreatureDTO> getCreaturesByWeapon(WeaponDTO weapon);
+	public Set<WeaponDTO> getAllWeapons();
 }

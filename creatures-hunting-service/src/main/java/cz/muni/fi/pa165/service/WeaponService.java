151c2319f219b9892871fd1ff5cf606d17764bbf
Ondrej Klein <ondra.klein@gmail.com>
2015-11-25 19:56:16 +0100

    multimodule project

--- creatures-hunting-service/src/main/java/cz/muni/fi/pa165/service/WeaponService.java
+++ creatures-hunting-service/src/main/java/cz/muni/fi/pa165/service/WeaponService.java
@@ -1,13 +1,18 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package cz.muni.fi.pa165.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
 
 /**
  *
- * @author ondra
+ * @author Ondrej Klein
  */
+@Service
 public interface WeaponService {
-	
+	public Weapon findWeapon(Long id);
+    public void addWeapon(Weapon weapon);
+    public void deleteWeapon(Weapon weapon);
+    public void updateWeapon(Weapon weapon);
+    public List<Weapon> findAllWeapons();
 }

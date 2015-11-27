20beaa991bc48d173538d962909c123a2e6422e7
kizivat <david@kizivat.eu>
2015-11-27 23:41:09 +0100

    Missing JavaDocs

--- creatures-hunting-api/src/main/java/cz/muni/fi/pa165/facade/WeaponFacade.java
+++ creatures-hunting-api/src/main/java/cz/muni/fi/pa165/facade/WeaponFacade.java
@@ -9,13 +9,71 @@ import java.util.List;
  * @author Ondrej Klein
  */
 public interface WeaponFacade {
+	/**
+	 * Adds a weapon record.
+	 *
+	 * @param weapon instance of the weapon to be added
+	 */
 	public Long addWeapon(WeaponDTO weapon);
+
+	/**
+	 * Updates chosen weapon's column's values.
+	 *
+	 * @param weapon instance of the weapon to be updated featuring new data values
+	 */
 	public void updateWeapon(WeaponDTO weapon);
+
+	/**
+	 * Assigns a creature to a weapon.
+	 *
+	 * @param weapon weapon to assign
+	 * @param creature creature to assign
+	 */
 	public void assignCreature(WeaponDTO weapon, CreatureDTO creature);
+
+	/**
+	 * Unlists a creature as being weak against the weapon.
+	 *
+	 * @param weapon weapon to have a creature unlisted
+	 * @param creature creature to have a weapon unlisted
+	 */
 	public void removeCreature(WeaponDTO weapon, CreatureDTO creature);
+
+	/**
+	 * Removes a weapon record.
+	 *
+	 * @param weapon instance of the weapon to be removed
+	 */
 	public void removeWeapon(WeaponDTO weapon);
+
+	/**
+	 * Find a weapon by its ID in Weapons table.
+	 *
+	 * @param id ID of the weapon that is being searched for
+	 * @return weapon with she sufficient ID
+	 */
 	public WeaponDTO getWeaponById(Long id);
+
+	/**
+	 * Find a weapon by its name in Weapons table.
+	 *
+	 * @param name name of the weapon that is being searched for
+	 * @return weapon with she sufficient ID
+	 */
 	public WeaponDTO getWeaponByName(String name);
+
+	/**
+	 * Finds weapons that is the given creature weak against.
+	 *
+	 * @param creature creature to consider
+	 * @return list of weapons the creature is weak against
+	 */
 	public List<WeaponDTO> getWeaponsByCreature(CreatureDTO creature);
+
+	/**
+	 * Lists all weapons.
+	 *
+	 * @return list of all weapons
+	 */
 	public List<WeaponDTO> getAllWeapons();
 }
--- creatures-hunting-service/src/main/java/cz/muni/fi/pa165/service/WeaponService.java
+++ creatures-hunting-service/src/main/java/cz/muni/fi/pa165/service/WeaponService.java
@@ -14,13 +14,71 @@ import org.springframework.stereotype.Service;
  */
 
 public interface WeaponService {
+	/**
+	 * Retrieves weapon with given id.
+	 * @param id id of weapon to find
+	 * @return weapon with given id
+	 */
 	Weapon findWeaponById(Long id);
+
+	/**
+	 * Finds weapon by its name
+	 *
+	 * @param name name of the weapon to be found
+	 * @return weapon with given name
+	 */
 	Weapon findWeaponByName(String name);
+
+	/**
+	 * Finds weapons that is the given creature weak against.
+	 *
+	 * @param creature creature to consider
+	 * @return list of weapons the creature is weak against
+	 */
 	List<Weapon> findWeaponsByCreature(Creature creature);
+
+	/**
+	 * Assigns a creature to a weapon.
+	 *
+	 * @param weapon weapon to assign
+	 * @param creature creature to assign
+	 */
 	void assignCreature(Weapon weapon, Creature creature);
+
+	/**
+	 * Unlists a creature as being weak against the weapon.
+	 *
+	 * @param weapon weapon to have a creature unlisted
+	 * @param creature creature to have a weapon unlisted
+	 */
 	void removeCreature(Weapon weapon, Creature creature);
+
+	/**
+	 * Creates a weapon.
+	 *
+	 * @param weapon instance of the weapon to be created
+	 * @return instance of the weapon created
+	 */
     Weapon addWeapon(Weapon weapon);
+
+	/**
+	 * Deletes a weapon.
+	 *
+	 * @param weapon instance of the weapon to be deleted
+	 */
     void deleteWeapon(Weapon weapon);
+
+	/**
+	 * Updates the weapon info.
+	 *
+	 * @param weapon instance of the weapon having new info
+	 */
     void updateWeapon(Weapon weapon);
+
+	/**
+	 * Lists all weapons.
+	 *
+	 * @return list of all weapons
+	 */
     List<Weapon> findAllWeapons();
 }

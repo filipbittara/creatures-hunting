3466082e8ba41aefb3d83aac444fa69786acc580
Ondrej Klein <ondra.klein@gmail.com>
2015-12-10 19:18:03 +0100

    mvc kick-off

---
+++ creatures-hunting-mvc/src/test/java/cz/muni/fi/pa165/mvc/controllers/WeaponControllerTest.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.mvc.controllers;
+
+/**
+ *
+ * @author klein
+ */
+public class WeaponControllerTest {
+    
+}

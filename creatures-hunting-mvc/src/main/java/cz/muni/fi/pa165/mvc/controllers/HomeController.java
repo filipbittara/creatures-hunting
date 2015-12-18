21c0d9ca8d356ac4113197f08f4a240102b3045c
Ondrej Klein <ondra.klein@gmail.com>
2015-12-18 16:22:28 +0100

    correct function of home link

--- creatures-hunting-mvc/src/main/java/cz/muni/fi/pa165/mvc/controllers/HomeController.java
+++ creatures-hunting-mvc/src/main/java/cz/muni/fi/pa165/mvc/controllers/HomeController.java
@@ -1,5 +1,9 @@
 package cz.muni.fi.pa165.mvc.controllers;
 
+import cz.muni.fi.pa165.dto.UserDTO;
+import cz.muni.fi.pa165.facade.UserFacade;
+import javax.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +16,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/home")
 public class HomeController {
-    @RequestMapping(method=RequestMethod.GET)
+    @Autowired
+    UserFacade userFacade;
+    
+    @Autowired
+    HttpSession session;
+    
+    @RequestMapping(method = RequestMethod.GET)
     public String home(Model model) {
+        UserDTO user = UserDTO.class.cast(session.getAttribute("authenticated"));
+        if (user != null) {
+            if (userFacade.isAdmin(user)) {
+                model.addAttribute("authenticatedAdmin", user.getEmail());
+            } else {
+                model.addAttribute("authenticatedUser", user.getEmail());
+            }
+        }
         return "/home";
     }
 }
--- creatures-hunting-mvc/src/main/webapp/WEB-INF/tags/template.tag
+++ creatures-hunting-mvc/src/main/webapp/WEB-INF/tags/template.tag
@@ -30,7 +30,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="${pageContext.request.contextPath}">Creatures hunting</a>
+            <a class="navbar-brand" href="${pageContext.request.contextPath}/home/">Creatures hunting</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
             <ul class="nav navbar-nav">

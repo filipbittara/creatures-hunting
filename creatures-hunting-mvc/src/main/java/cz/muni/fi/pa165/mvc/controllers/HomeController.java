31d7d211368418f9a2f3606aa4c530afcd75afe7
filipbittara <409770@mail.muni.cz>
2016-01-14 22:07:47 +0100

    business method added to home tab

--- creatures-hunting-mvc/src/main/java/cz/muni/fi/pa165/mvc/controllers/HomeController.java
+++ creatures-hunting-mvc/src/main/java/cz/muni/fi/pa165/mvc/controllers/HomeController.java
@@ -1,11 +1,15 @@
 package cz.muni.fi.pa165.mvc.controllers;
 
+import cz.muni.fi.pa165.dto.CreatureDTO;
 import cz.muni.fi.pa165.dto.UserDTO;
+import cz.muni.fi.pa165.facade.AreaFacade;
+import cz.muni.fi.pa165.facade.CreatureFacade;
 import cz.muni.fi.pa165.facade.UserFacade;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -22,6 +26,9 @@ public class HomeController {
     @Autowired
     HttpSession session;
     
+    @Autowired
+    private CreatureFacade creatureFacade;
+    
     @RequestMapping(method = RequestMethod.GET)
     public String home(Model model) {
         UserDTO user = UserDTO.class.cast(session.getAttribute("authenticated"));
@@ -32,6 +39,19 @@ public class HomeController {
                 model.addAttribute("authenticatedUser", user.getUsername());
             }
         }
+        return "/home";
+    }
+    
+    @RequestMapping(value = {"/circle/{x}/{y}/{radius}"}, method = RequestMethod.GET)
+    public String circle(Model model, @PathVariable double x, @PathVariable double y, @PathVariable double radius) {
+        String creatures = "";
+        for (CreatureDTO c: creatureFacade.getCreaturesInCircle(y, x, radius)) {
+            creatures += c.getName() + ", ";
+        }
+        if(creatures.length() > 0) {
+            creatures = creatures.substring(0, creatures.length() - 2);
+        }
+        model.addAttribute("circle", creatures);
         return "/home";
     }
 }
--- creatures-hunting-mvc/src/main/webapp/WEB-INF/jsp/home.jsp
+++ creatures-hunting-mvc/src/main/webapp/WEB-INF/jsp/home.jsp
@@ -13,6 +13,31 @@
 
 <my:template title="Home">
 <jsp:attribute name="body">
-    <h2>Home</h2>
+    <script  type="text/javascript">
+        function submit_circle() {
+            if(event.keyCode == 13) {
+                location.href = '${pageContext.request.contextPath}' + '/home/circle/' + filter_page.value + '/' + '/'        
+            }
+        }
+    </script>
+    <div class="input-group col-md-6">
+        <div>
+            <label cssClass="col-sm-2 control-label">Longitude</label>
+            <input type="text" id="x" class="form-control" value="${x}"/>
+        </div>
+        <div>
+            <label cssClass="col-sm-2 control-label">Latitude</label>
+            <input type="text" id="y" class="form-control" value="${y}"/>
+        </div>
+        <div>
+            <label cssClass="col-sm-2 control-label">Radius</label>
+            <input type="text" id="radius" class="form-control" value="${radius}"/>
+        </div>
+        <input type="button" value="Show" class="btn btn-primary" onclick="location.href = '${pageContext.request.contextPath}' + '/home/circle/' + x.value + '/'+ y.value + '/' + radius.value"/>
+    </div>
+    <c:if test="${circle != '' && circle != null}">
+        <br />
+        <b>Creatures in given circle: ${circle}.</b>
+    </c:if>
 </jsp:attribute>
 </my:template>
